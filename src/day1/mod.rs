1610f95f65da0145d32719579cada7f557570748
Kellie Medlin <kellie.medlin@gmail.com>
2020-12-01 13:46:12 -0800

    Day 1, Part 2, but without vector nonsense

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,35 +1,46 @@
-use arr_macro::arr;
-
 pub fn report_repair_p1(input: &[usize]) -> Option<u64> {
     let mut cache: [bool; 2021] = [false; 2021];
     for element in input.iter() {
-        cache[*element] = true;
         let partner = 2020 - element;
         if cache[partner] {
             return Some((element * partner) as u64);
         }
+        cache[*element] = true;
     }
     return None;
 }
 
 pub fn report_repair_p2(input: &[usize]) -> Option<u64> {
-    let mut cache: [Vec<(usize, usize)>; 2021] = arr!(vec![]; 2021);
-    // O(n^2)
-    for i in 0..input.len() {
-        for j in (i+1)..input.len() {
-            let candidate_index = input[i] + input[j];
-            if candidate_index < cache.len() {
-                cache[candidate_index].push((i, j));
-            }
+    let mut count_cache: [u8; 2021] = [0; 2021];
+
+    // Cases
+    // 3*x = 2020
+    // 2*x + y = 2020
+    // x + y + z = 2020
+    //
+    // The first situation is impossible, as 2020 is not cleanly divisible by 3.
+    // We consider the other two cases below.
+    for element in input.iter() {
+        let mut count = count_cache[*element];
+        if count < 2 {
+            count_cache[*element] = count + 1;
         }
     }
 
-    // O(n)
-    for (i, element) in input.iter().enumerate() {
-        let candidates = &cache[2020 - element];
-        for indices in candidates.iter() {
-            if indices.0 != i && indices.1 != i {
-                return Some((element * input[indices.0] * input[indices.1]) as u64);
+    // O(n^2)
+    for i in 0..input.len() {
+        for j in (i + 1)..input.len() {
+            let candidate_sum = input[i] + input[j];
+            if candidate_sum <= 2020 {
+                let candidate_input = 2020 - candidate_sum;
+                let required_count = if candidate_input == input[j] || candidate_input == input[i] {
+                    2
+                } else {
+                    1
+                };
+                if count_cache[candidate_input] >= required_count {
+                    return Some((candidate_input * input[i] * input[j]) as u64);
+                }
             }
         }
     }
